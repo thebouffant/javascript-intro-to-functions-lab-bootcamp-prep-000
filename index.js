c7b92469b99bd9c696da55946c3dc6c0f1b0a8fc
Marc <marc.troughton@gmail.com>
2018-09-07 01:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function sayHiToGrandma(string) {
  if (string.toLowerCase() === string) {
    return "I can\'t hear you!"
  }
- elseif (string.toUpperCase() === string) {
-   return ""
-  
-}
+ else if (string.toUpperCase() === string) {
+   return "YES INDEED!"
+ }
+ else if (string.)
